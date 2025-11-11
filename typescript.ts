bba639ff4b59ba821544ccbb1206c7ff1f4b8e7f
hasanaat <arsheema619@gmail.com>
2025-11-11 21:41:12 +0530

    ts if much fun

--- typescript.ts
+++ typescript.ts
@@ -5,11 +5,7 @@ function add(a: number, b: number): number {
 console.log(add("2", 3)); // ❌ Error at compile time: Argument of type 'string' is not assignable to parameter of type 'number'
 
 //Why Developers Prefer TypeScript
-
 //Fewer bugs
-
 //Predictable behavior
-
 //Easier to collaborate in teams
-
 //Great for large, scalable applications (React, Angular, etc.)
