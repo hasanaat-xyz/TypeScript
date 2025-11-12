a8b5c28c0fa8152320b530027e785b7bbc7d690a
hasanaat <arsheema619@gmail.com>
2025-11-12 14:14:21 +0530

    enums

--- numericEnums.ts
+++ numericEnums.ts
@@ -1,10 +1,10 @@
-enum Direction {
+enum Direction 
+{
   Up,       // 0
   Down,     // 1
   Left,     // 2
   Right     // 3
 }
-
 console.log(Direction.Up);      // 0
 console.log(Direction.Right);   // 3
 
