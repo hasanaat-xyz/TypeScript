28a235550ab96c1d9809ddc7f4676aae1373a274
hasanaat <arsheema619@gmail.com>
2025-11-16 16:27:40 +0530

    ts file updated

--- typescript.ts
+++ typescript.ts
@@ -1,7 +1,6 @@
 function add1(a: number | string, b: number | string): number {
   return Number(a) + Number(b);
 }
-
 console.log(add1("2", 3)); // ✅ Output: 5
 console.log(add1("4", "6")); // ✅ Output: 10
 
